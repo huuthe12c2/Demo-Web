7d023aff64f46d3cc5f03495d9f19587f4810a14
huuthe12c2 <capnhatvipporo1@gmail.com>
2021-01-11 11:31:38 +0700

    first commit

---
+++ sss.php
@@ -0,0 +1,34 @@
+<div class="im-partner">
+    <div class="fusion-fullwidth fullwidth-box fusion-builder-row-5 fusion-flex-container nonhundred-percent-fullwidth non-hundred-percent-height-scrolling">
+        <div class="fusion-builder-row fusion-row fusion-flex-align-items-flex-start">
+            <div
+                class="fusion-layout-column fusion_builder_column fusion-builder-column-17 fusion_builder_column_1_1 1_1 fusion-flex-column">
+                <div class="fusion-column-wrapper fusion-flex-justify-content-flex-start fusion-content-layout-column">
+                    <div
+                        class="fusion-title title fusion-title-4 fusion-sep-none fusion-title-center fusion-title-text fusion-title-size-two fusion-border-below-title">
+                        <h4 class="title-heading-center fusion-responsive-typography-calculated"
+                            style="text-align: center;" data-fontsize="32" data-lineheight="38.4px"><b>ĐỐI TÁC THỰC
+                                HIỆN</b></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div
+        class="fusion-fullwidth fullwidth-box fusion-builder-row-6 fusion-flex-container nonhundred-percent-fullwidth non-hundred-percent-height-scrolling">
+        <div class="fusion-builder-row fusion-row fusion-flex-align-items-flex-start">
+            <div
+                class="fusion-layout-column fusion_builder_column fusion-builder-column-18 fusion_builder_column_1_1 1_1 fusion-flex-column">
+                <div class="fusion-column-wrapper fusion-flex-justify-content-flex-start fusion-content-layout-column">
+                    <div class="fusion-text fusion-text-1">
+                        <p style="text-align: center;">Bạn đang cần tư vấn để xây dựng một hệ thống đầu tư MLM dành
+                            riêng cho doanh nghiệp của bạn.
+                            Hãy liên hệ với chúng tôi!</p>
+                        <p style="text-align: center;">Website: sellcodemlm.com</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
